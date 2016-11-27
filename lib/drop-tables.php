d7a9f423a06e96c19d421181fff6c769404b5ece
Berry Plasman <info@berryplasman.com>
2016-11-27 22:01:46 +0100

    somes testing vars

--- lib/drop-tables.php
+++ lib/drop-tables.php
@@ -1,11 +1,17 @@
-<?php global $wpdb; ?>
+<?php
+    global $wpdb;
+    // die($wpdb->prefix);
 
+    $drop_cities = 'DROP TABLE ' . $wpdb->prefix . 'cities;';
+    // die($drop_cities);
+    echo $drop_cities;
+
+/*
 DROP TABLE IF EXISTS <?php echo $wpdb->prefix; ?>cities;
-
-<?php /*
 DROP TABLE IF EXISTS <?php echo $wpdb->prefix; ?>countries;
 DROP TABLE IF EXISTS <?php echo $wpdb->prefix; ?>provences;
 DROP TABLE IF EXISTS sd8_cities;
 DROP TABLE IF EXISTS sd8_countries;
 DROP TABLE IF EXISTS sd8_provences;
-*/ ?>
+*/
+?>
