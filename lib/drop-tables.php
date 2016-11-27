6124d2e33664cc13fe84f6fc73229573d16fbef0
Berry Plasman <info@berryplasman.com>
2016-11-27 14:55:47 +0100

    import/delete city database

---
+++ lib/drop-tables.php
@@ -0,0 +1,11 @@
+<?php global $wpdb; ?>
+
+DROP TABLE IF EXISTS <?php echo $wpdb->prefix; ?>cities;
+
+<?php /*
+DROP TABLE IF EXISTS <?php echo $wpdb->prefix; ?>countries;
+DROP TABLE IF EXISTS <?php echo $wpdb->prefix; ?>provences;
+DROP TABLE IF EXISTS sd8_cities;
+DROP TABLE IF EXISTS sd8_countries;
+DROP TABLE IF EXISTS sd8_provences;
+*/ ?>
---
+++ lib/prepare-tables.php
@@ -0,0 +1,18 @@
+<?php global $wpdb; ?>
+
+#
+# Table structure for table 'cities'
+#
+
+DROP TABLE IF EXISTS <?php echo $wpdb->prefix; ?>cities;
+CREATE TABLE <?php echo $wpdb->prefix; ?>cities (
+    id int(4) unsigned NOT NULL auto_increment,
+    city_name_ascii varchar(50) NULL,
+    state_code varchar(4) NULL,
+    states varchar(50) NULL,
+    country_code varchar(3) NULL,
+    country varchar(50) NULL,
+    PRIMARY KEY (id)
+) ;
+
+INSERT INTO <?php echo $wpdb->prefix; ?>cities ( city_name_ascii, state_code, states, country_code, country ) VALUES( "Amsterdam", "NH", "Noord-Holland", "NL", "Netherlands" );
