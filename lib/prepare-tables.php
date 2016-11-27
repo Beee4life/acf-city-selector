03fa552aaf6b57fde8d62ac0e4066915b524e73d
Berry Plasman <info@berryplasman.com>
2016-11-27 18:06:18 +0100

    change database structure

--- lib/prepare-tables.php
+++ lib/prepare-tables.php
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS <?php echo $wpdb->prefix; ?>cities;
 CREATE TABLE <?php echo $wpdb->prefix; ?>cities (
     id int(4) unsigned NOT NULL auto_increment,
     city_name_ascii varchar(50) NULL,
-    state_code varchar(4) NULL,
+    state_code varchar(3) NULL,
     states varchar(50) NULL,
     country_code varchar(3) NULL,
     country varchar(50) NULL,
