ea7f04bf5e8da62e689390ec1e1fd2cc6c4ab97f
Beee <info@berryplasman.com>
2017-09-22 02:25:07 +0200

    Clean up

--- inc/settings-page.php
+++ inc/settings-page.php
@@ -8,7 +8,7 @@
 			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 		}
 
-		acf_plugin_city_selector::al_show_admin_notices();
+		acf_plugin_city_selector::acfcs_show_admin_notices();
 
 		if ( isset( $_POST["db_actions_nonce"] ) ) {
 			if ( ! wp_verify_nonce( $_POST["db_actions_nonce"], 'db-actions-nonce' ) ) {
@@ -31,9 +31,10 @@
             <div id="icon-options-general" class="icon32"><br /></div>
 
             <h1><?php esc_html_e( 'ACF City Selector Settings', 'acf-city-selector' ); ?></h1>
-            <p><?php sprintf( esc_html__( 'On this page you can find some helpful info about the %s plugin as well as some settings.', 'acf-city-selector' ), 'ACF City Selector' ); ?></p>
 
-            <p><a href="<?php echo site_url(); ?>/wp-admin/options-general.php?page=acfcs-options">Dashboard</a> | <a href="<?php echo site_url(); ?>/wp-admin/options-general.php?page=acfcs-settings">Settings</a></p>
+			<?php echo acf_plugin_city_selector::acfcs_admin_menu(); ?>
+
+            <p><?php sprintf( esc_html__( 'On this page you can find some helpful info about the %s plugin as well as some settings.', 'acf-city-selector' ), 'ACF City Selector' ); ?></p>
 
             <!-- left part -->
             <div class="admin_left">
@@ -76,28 +77,8 @@
 
             </div><!-- end .admin_left -->
 
-            <div class="admin_right">
+			<?php include( 'admin-right.php' ); ?>
 
-                <h3><?php esc_html_e( 'About the plugin', 'acf-city-selector' ); ?></h3>
-                <p><?php echo sprintf( __( 'This plugin is an extension for %s. I built it because there was no properly working plugin which did this.', 'acf-city-selector' ), '<a href="https://www.advancedcustomfields.com/" target="_blank">Advanced Custom Fields</a>' ); ?>
-                <p><?php echo sprintf( __( '<a href="%s" target="_blank">Click here</a> for a demo on my own website.', 'acf-city-selector' ), esc_url( 'http://www.berryplasman.com/wordpress/acf-city-selector/?utm_source=wpadmin&utm_medium=about_plugin&utm_campaign=acf-plugin' ) ); ?></p>
-
-                <hr />
-
-                <h3><?php esc_html_e( 'About Beee', 'acf-city-selector' ); ?></h3>
-                <p><?php esc_html_e( 'If you need a Wordpress designer/coder to do work on your site, hit me up.', 'acf-city-selector' ); ?></p>
-
-                <hr />
-
-                <h3>Support</h3>
-                <p><?php echo sprintf( __( 'If you need support for this plugin or if you have some good suggestions for improvements and/or new features, please turn to %s.', 'acf-city-selector' ), '<a href="https://github.com/Beee4life/acf-city-selector/issues" target="_blank">Github</a>' ); ?>
-                </p>
-                <hr />
-
-                <p><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=24H4ULSQAT9ZL" target="_blank"><img src="<?php echo plugins_url( 'assets/img/paypal_donate.gif', __FILE__ ); ?>" alt="" class="donateimg" /></a>
-                    <?php esc_html_e( 'If you like this plugin, buy me a coke to show your appreciation so I can continue to develop it.', 'acf-city-selector' ); ?></p>
-
-            </div><!-- end .admin_right -->
         </div><!-- end .wrap -->
 		<?php
 	}
