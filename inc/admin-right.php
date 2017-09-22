1423001183ee81855e880298423f53aafad95a8c
Beee <info@berryplasman.com>
2017-09-22 02:23:36 +0200

    Make include of sidebar

---
+++ inc/admin-right.php
@@ -0,0 +1,22 @@
+<div class="admin_right">
+
+    <h3><?php esc_html_e( 'About the plugin', 'acf-city-selector' ); ?></h3>
+    <p><?php echo sprintf( __( 'This plugin is an extension for %s. I built it because there was no properly working plugin which did this.', 'acf-city-selector' ), '<a href="' . esc_url( 'https://www.advancedcustomfields.com/' ) . '" target="_blank">Advanced Custom Fields</a>' ); ?>
+    <p><?php echo sprintf( __( '<a href="%s" target="_blank">Click here</a> for a demo on my own website.', 'acf-city-selector' ), esc_url( 'http://www.berryplasman.com/wordpress/acf-city-selector/?utm_source=wpadmin&utm_medium=about_plugin&utm_campaign=acf-plugin' ) ); ?></p>
+
+    <hr />
+
+    <h3><?php esc_html_e( 'About Beee', 'acf-city-selector' ); ?></h3>
+    <p><?php esc_html_e( 'If you need a Wordpress designer/coder to do work on your site, hit me up.', 'acf-city-selector' ); ?></p>
+
+    <hr />
+
+    <h3>Support</h3>
+    <p><?php echo sprintf( __( 'If you need support for this plugin or if you have some good suggestions for improvements and/or new features, please turn to %s.', 'acf-city-selector' ), '<a href="https://github.com/Beee4life/acf-city-selector/issues" target="_blank">Github</a>' ); ?>
+    </p>
+    <hr />
+
+    <p><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=24H4ULSQAT9ZL" target="_blank"><img src="<?php echo plugins_url( '/assets/img/paypal_donate.gif', dirname( __FILE__ )  ); ?>" alt="" class="donateimg" /></a>
+		<?php esc_html_e( 'If you like this plugin, buy me a coke to show your appreciation so I can continue to develop it.', 'acf-city-selector' ); ?></p>
+
+</div><!-- end .admin_right -->
