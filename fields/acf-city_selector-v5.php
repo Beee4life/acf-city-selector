ab360db1810a5035b287456663dbaa2e467a3802
Beee <info@berryplasman.com>
2017-09-22 22:10:38 +0200

    Improve strings + if statements

--- fields/acf-city_selector-v5.php
+++ fields/acf-city_selector-v5.php
@@ -104,18 +104,20 @@
                 $stateCode   = $field[ 'value' ][ 'stateCode' ];
                 $cityName    = $field[ 'value' ][ 'cityNameAscii' ];
                 $states      = get_states( $countrycode );
-                $stateName   = $states[ substr( $stateCode, 3 ) ];
+                $stateName   = ! empty( $states ) ? $states[ substr( $stateCode, 3 ) ] : false;
 				?>
                 <div class="cs_countries">
 					<?php if ( $field['show_labels'] == 1 ) { ?>
-                        <span class="acf-input-header"><?php _e( 'Select country', 'acf-city-selector' ); ?></span>
+                        <span class="acf-input-header"><?php esc_html_e( 'Select country', 'acf-city-selector' ); ?></span>
 					<?php } ?>
                     <label for="countryCode" class="screen-reader-text"></label>
                     <select name="acf[<?php echo $field['key']; ?>][countryCode]" id="countryCode" class="countrySelect">
-                        <optgroup label="<?php _e( 'Current option', 'acf-city-selector' ); ?>">
-                            <option value="<?php echo $countrycode; ?>"><?php echo $countries[$countrycode]; ?></option>
-                        </optgroup>
-						<?php
+                        <?php if ( $countrycode != null ) { ?>
+                            <optgroup label="<?php esc_html_e( 'Current option', 'acf-city-selector' ); ?>">
+                                <option value="<?php echo $countrycode; ?>"><?php echo $countries[ $countrycode ]; ?></option>
+                            </optgroup>
+                        <?php
+                        }
                             foreach ( populate_country_select( '', $field ) as $key => $country ) {
                                 if ( $countrycode == $key ) {
                                     $selected = ' selected';
@@ -132,11 +134,11 @@
 
                 <div class="cs_provences">
 					<?php if ( $field['show_labels'] == 1 ) { ?>
-                        <span class="acf-input-header"><?php _e( 'Select provence/state', 'acf-city-selector' ); ?></span>
+                        <span class="acf-input-header"><?php esc_html_e( 'Select provence/state', 'acf-city-selector' ); ?></span>
 					<?php } ?>
                     <label for="stateCode" class="screen-reader-text"></label>
                     <select name="acf[<?php echo $field['key']; ?>][stateCode]" id="stateCode" class="countrySelect">
-                        <optgroup label="<?php _e( 'Current option', 'acf-city-selector' ); ?>">
+                        <optgroup label="<?php esc_html_e( 'Current option', 'acf-city-selector' ); ?>">
                             <option value="<?php echo $stateCode; ?>"><?php echo $stateName; ?></option>
                         </optgroup>
                     </select>
@@ -177,14 +179,14 @@
 				wp_register_script( 'acf-city-selector-js', "{$url}assets/js/city-selector.js", array( 'acf-input' ), $version );
 				wp_enqueue_script( 'acf-city-selector-js' );
 
-                if ( isset( $_GET['action'] ) && $_GET['action'] === 'edit' ) {
-                    $post_meta = get_post_meta( get_the_ID(), 'cities', 1 );
+				if ( isset( $_GET['action'] ) && $_GET['action'] === 'edit' ) {
+					$post_meta = get_post_meta( get_the_ID(), 'test_acf', 1 );
 
-                    wp_localize_script( 'acf-city-selector-js', 'city_selector_vars', array(
-                        'countryCode' => $post_meta[ 'countryCode' ],
-                        'stateCode'   => $post_meta[ 'stateCode' ],
-                    ) );
-                }
+					wp_localize_script( 'acf-city-selector-js', 'city_selector_vars', array(
+						'countryCode' => $post_meta[ 'countryCode' ],
+						'stateCode'   => $post_meta[ 'stateCode' ],
+					) );
+				}
 
 				// register & include CSS
 				wp_register_style( 'acf-city-selector-css', "{$url}assets/css/acf-city-selector.css", array( 'acf-input' ), $version );
