5ef8a7077ab8c1e068c6a73493d62b950309f191
Beee <info@berryplasman.com>
2017-09-23 17:21:25 +0200

    Docblocks + refactor

--- inc/country-field.php
+++ inc/country-field.php
@@ -1,8 +1,8 @@
 <?php
+
 	/**
 	 * Set admin-ajax.php on the front side (by default it is available only for Backend)
 	 */
-
 	function city_selector_ajaxurl() {
 		?>
         <script type="text/javascript">
@@ -12,11 +12,16 @@
 	}
 	add_action( 'wp_head', 'city_selector_ajaxurl' );
 
-	/*
-	 * Fill the countries select
-	 * @return Array
+	/**
+     * Fill the countries select
+     *
+	 * @param null $selectedCountry
+	 * @param $field
+	 *
+	 * @return array
 	 */
 	function populate_country_select( $selectedCountry = null, $field ) {
+
 		global $wpdb;
 		$db = $wpdb->get_results( "
         SELECT * FROM " . $wpdb->prefix . "cities
@@ -66,16 +71,17 @@
         $db = $wpdb->get_results( $sql );
 
         foreach ( $db as $data ) {
-            $items[ $data->state_code ]   = $data->states;
+            $items[ $data->state_code ] = $data->states;
         }
         return $items;
     }
 
 	/*
-	 * Get states by related Country Code
-	 * @return JSON Object
+     * Get states by related Country Code
+     *
+	 * @param bool $country_code
+     * @return JSON Object
 	 */
-
 	function get_states_call( $country_code = false ) {
 
 		if ( ! $country_code ) {
@@ -92,10 +98,13 @@
         $db = $wpdb->get_results( $sql );
 
 		$items                    = array();
-		// $items[0]['country_code'] = "";
-		// $items[0]['state_code']   = "";
-		// $items[0]['states']       = __( 'Select provence/state', 'acf-city-selector' );
-		$i                        = 0;
+		$items[0]['country_code'] = "";
+		$items[0]['state_code']   = "";
+		$items[0]['states']       = __( 'Select provence/state', 'acf-city-selector' );
+		$i                        = 1;
+
+        // @TODO: check if $field['show_labels'] == 1
+        // if == 1, $items[0]['states'] = '-';
 
 		foreach ( $db as $data ) {
 			$items[ $i ]['country_code'] = $data->country_code;
@@ -107,7 +116,6 @@
 			}
 			$i ++;
 		}
-		// return $items;
 		ob_clean();
 		echo json_encode( $items );
 		die();
@@ -115,6 +123,7 @@
 
 	/*
 	 * Get cities by related State Code or Country Code (IF State code == "00" or States == 'N/A')
+	 *
 	 * @return JSON Object
 	 */
 	function get_cities_call() {
@@ -140,24 +149,19 @@
             " );
 			}
 			$items                 = array();
-			// $items[0]['id']        = "";
-			// $items[0]['city_name'] = __( 'Select city', 'acf-city-selector' );
-			$i                     = 0;
+			$items[0]['id']        = "";
+			$items[0]['city_name'] = __( 'Select city', 'acf-city-selector' );
+			$i                     = 1;
 
 			foreach ( $db as $data ) {
 				$items[ $i ]['id']        = $data->state_code;
 				$items[ $i ]['city_name'] = $data->city_name_ascii;
 				$i ++;
 			}
-			// return $items;
 			ob_clean();
 			echo json_encode( $items );
 			die();
 		}
-	}
-
-	function get_selected_city_call() {
-	    // echo '<pre>'; var_dump($_POST); echo '</pre>'; exit;
 	}
 
 	add_action( 'wp_ajax_get_states_call', 'get_states_call' );
