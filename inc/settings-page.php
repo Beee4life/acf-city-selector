1c216b485e39c38e8885aa5083df7ada1954280f
Beee <info@berryplasman.com>
2017-09-24 02:30:35 +0200

    Simplify forms

--- inc/settings-page.php
+++ inc/settings-page.php
@@ -10,21 +10,6 @@
 
 		acf_plugin_city_selector::acfcs_show_admin_notices();
 
-		if ( isset( $_POST["db_actions_nonce"] ) ) {
-			if ( ! wp_verify_nonce( $_POST["db_actions_nonce"], 'db-actions-nonce' ) ) {
-				return;
-			} else {
-				if ( isset( $_POST['import_nl'] ) && 1 == $_POST["import_nl"] ) {
-					echo '<div class="updated"><p><strong>' . __( 'You successfully imported all cities in The Netherlands.', 'acf-city-selector' ) . '</strong></p></div>';
-				}
-				if ( isset( $_POST['import_be'] ) && 1 == $_POST["import_be"] ) {
-					echo '<div class="updated"><p><strong>' . __( 'You successfully imported all cities in Belgium.', 'acf-city-selector' ) . '</strong></p></div>';
-				}
-				if ( isset( $_POST['import_lux'] ) && 1 == $_POST["import_lux"] ) {
-					echo '<div class="updated"><p><strong>' . __( 'You successfully imported all cities in Luxembourg.', 'acf-city-selector' ) . '</strong></p></div>';
-				}
-			}
-		}
 		?>
 
 		<div class="wrap">
@@ -46,27 +31,38 @@
                     <p><?php esc_html_e( "Here you can (re-)import all cities for the individual countries listed below.", 'acf-city-selector' ); ?></p>
 
                     <p>
-                        <span class="acfcs_input">
-                            <label for="import_nl" class="screen-reader-text"></label>
-                            <input type="checkbox" name="import_nl" id="import_nl" value="1" /> <?php esc_html_e( 'Import cities in Holland/The Netherlands', 'acf-city-selector' ); ?> (2449)
-                        </span>
+                        <label for="import_nl" class="screen-reader-text"></label>
+                        <input type="checkbox" name="import_nl" id="import_nl" value="1" /> <?php esc_html_e( 'Import cities in Holland/The Netherlands', 'acf-city-selector' ); ?> (2449)
                     </p>
 
                     <p>
-                        <span class="acfcs_input">
-                            <label for="import_be" class="screen-reader-text"></label>
-                            <input type="checkbox" name="import_be" id="import_be" value="1" /> <?php esc_html_e( 'Import cities in Belgium', 'acf-city-selector' ); ?> (1166)
-                        </span>
+                        <label for="import_be" class="screen-reader-text"></label>
+                        <input type="checkbox" name="import_be" id="import_be" value="1" /> <?php esc_html_e( 'Import cities in Belgium', 'acf-city-selector' ); ?> (1166)
                     </p>
 
                     <p>
-                        <span class="acfcs_input">
-                            <label for="import_lux" class="screen-reader-text"></label>
-                            <input type="checkbox" name="import_lux" id="import_lux" value="1" /> <?php esc_html_e( 'Import cities in Luxembourg', 'acf-city-selector' ); ?> (12)
-                        </span>
+                        <label for="import_lux" class="screen-reader-text"></label>
+                        <input type="checkbox" name="import_lux" id="import_lux" value="1" /> <?php esc_html_e( 'Import cities in Luxembourg', 'acf-city-selector' ); ?> (12)
                     </p>
 
                     <input name="" type="submit" class="button button-primary" value="<?php esc_html_e( 'Import selected countries', 'acf-city-selector' ); ?>" />
+                </form>
+
+
+                <br /><hr />
+
+                <form method="post" action="">
+                    <input name="truncate_table_nonce" value="<?php echo wp_create_nonce( 'truncate-table-nonce' ); ?>" type="hidden" />
+
+                    <h2><?php esc_html_e( 'Clear the database', 'acf-city-selector' ); ?></h2>
+                    <p><?php esc_html_e( "By selecting this option, you will remove all cities, which are present in the database. This is handy if you don't need the preset cities or you want a fresh start.", 'acf-city-selector' ); ?></p>
+
+                    <p>
+                        <label for="delete_cities" class="screen-reader-text"></label>
+                        <input type="checkbox" name="delete_cities" id="delete_cities" value="1" /> <?php esc_html_e( 'Delete all cities from the database', 'acf-city-selector' ); ?>
+                    </p>
+
+                    <input name="" type="submit" class="button button-primary"  onclick="return confirm( 'Are you sure you want to delete all cities ?' )" value="<?php esc_html_e( 'Nuke \'em', 'acf-city-selector' ); ?>" />
                 </form>
 
                 <br /><hr />
@@ -86,25 +82,6 @@
                     </p>
 
                     <input name="" type="submit" class="button button-primary" value="<?php esc_html_e( 'Save settings', 'acf-city-selector' ); ?>" />
-                </form>
-
-                <br /><hr />
-
-                <form method="post" action="">
-                    <input name="truncate_table_nonce" value="<?php echo wp_create_nonce( 'truncate-table-nonce' ); ?>" type="hidden" />
-
-                    <h2><?php esc_html_e( 'Clear the database', 'acf-city-selector' ); ?></h2>
-                    <p><?php esc_html_e( "By selecting this option, you will remove all cities, which are present in the database. This is handy if you don't need the preset cities or you want a fresh start.", 'acf-city-selector' ); ?></p>
-
-                    <p>
-                        <span class="acfcs_input">
-                            <label for="delete_cities" class="screen-reader-text"></label>
-                            <input type="checkbox" name="delete_cities" id="delete_cities" value="1" /> <?php esc_html_e( 'Delete all cities from the database', 'acf-city-selector' ); ?>
-                        </span>
-                    </p>
-
-                    <input name="" type="submit" class="button button-primary"  onclick="return confirm( 'Are you sure you want to delete all cities ?' )" value="<?php esc_html_e( 'Nuke \'em', 'acf-city-selector' ); ?>" />
-                    <?php //submit_button(); ?>
                 </form>
 
             </div><!-- end .admin_left -->
