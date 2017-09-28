af2c6a8f0f393f2490f1e5eca3f0cbb9c053799a
Beee <info@berryplasman.com>
2017-09-29 00:56:52 +0200

    Add deps for css

--- acf-city-selector.php
+++ acf-city-selector.php
@@ -516,7 +516,7 @@
 			 * Adds CSS on the admin side
 			 */
 			public function acfcs_add_css() {
-				wp_enqueue_style( 'acf-city-selector', plugins_url( 'assets/css/acf-city-selector.css', __FILE__ ) );
+				wp_enqueue_style( 'acf-city-selector', plugins_url( 'assets/css/acf-city-selector.css', __FILE__ ), array( 'acf-input' ) );
 			}
 		}
 
