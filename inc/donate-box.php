14f4e55e057e73056d287614f81df0c212286304
Beee <info@berryplasman.com>
2019-09-14 18:13:17 +0200

    Fix URL

--- inc/donate-box.php
+++ inc/donate-box.php
@@ -6,7 +6,7 @@
 			}
 
 			$id       = 'donate-acf-cs';
-			$title    = '<a style="text-decoration: none; font-size: 1em;" href="https://github.com/beee4life" target="_blank" rel="noopener">' . sprintf( esc_html__( '%s says "Thank you"', 'acf-city-selector' ), 'Beee' ) . '</a>';
+			$title    = sprintf( esc_html__( '%s says "Thank you"', 'acf-city-selector' ), 'Beee' );
 			$callback = 'show_donate_meta_box';
 			$screens  = array();
 			$context  = 'side';
@@ -17,7 +17,7 @@
 		add_action( 'add_meta_boxes', 'acfcs_donate_meta_box' );
 
 		function show_donate_meta_box() {
-			echo '<p style="margin-bottom: 0;">' . sprintf( __( 'Thank you for installing the \'City Selector\' plugin. I hope you enjoy it. Please <a href="%s" target="_blank">consider a donation</a> if you do, so I can continue to improve it even more.', 'acf-city-selector' ), esc_url( 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=24H4ULSQAT9ZL' ) ) . '</p>';
+			echo '<p style="margin-bottom: 0;">' . sprintf( __( 'Thank you for installing the \'City Selector\' plugin. I hope you enjoy it. Please <a href="%s" rel="noopener" target="_blank">consider a donation</a> if you do, so I can continue to improve it even more.', 'acf-city-selector' ),  'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=24H4ULSQAT9ZL' ) . '</p>';
 		}
 	} // end if !function_exists
 
