6a1bf0094da33da2306b4b619a9d05195fe7275d
Berry Plasman <berry@totaldesign.com>
2020-05-06 01:01:52 +0200

    Remove help tab

--- inc/acfcs-help-tabs.php
+++ inc/acfcs-help-tabs.php
@@ -76,19 +76,6 @@
                 ) );
             }
 
-            if ( 'settings_page_acfcs-settings' == $screen->id ) {
-                $screen->add_help_tab( array(
-                    'id'      => 'import-file',
-                    'title'   => esc_html__( 'Import preset countries', 'acf-city-selector' ),
-                    'content' => '<h5>Import preset countries</h5>
-                        <p>' . esc_html__( 'On this page you can (re-)import the countries which come with the plugin when it\'s installed; Netherlands, Belgium and Luxembourg (if needed).', 'acf-city-selector' ) . '</p>
-                        <h5>Clear database</h5>
-                        <p>' . esc_html__( 'There\'s also an option to delete all cities, which can be helpful if you activate the plugin a second time. Right now all cities are imported again if you activate the plugin. This will also happen if you still have all the cities in the database from a previous activation.', 'acf-city-selector' ) . '</p>
-                        <h5>Preserve settings</h5>
-                        <p>' . esc_html__( 'If you select preserve settings, all values will not be deleted from the database when the plugin is deleted.', 'acf-city-selector' ) . '</p>'
-                ) );
-            }
-
             get_current_screen()->set_help_sidebar(
                 '<p><strong>' . esc_html__( 'Author\'s website', 'acf-city-selector' ) . '</strong></p>
                 <p><a href="https://berryplasman.com?utm_source=' . $_SERVER[ 'SERVER_NAME' ] . '&utm_medium=plugin_admin&utm_campaign=free_promo">berryplasman.com</a></p>'
