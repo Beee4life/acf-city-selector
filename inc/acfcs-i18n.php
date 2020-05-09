f770f1dfef57b4bacddd0e38de0b0f5bac0a6de0
Berry Plasman <berry@totaldesign.com>
2020-05-09 21:40:36 +0200

    Add australia as i18n string

--- inc/acfcs-i18n.php
+++ inc/acfcs-i18n.php
@@ -1,2 +1,4 @@
 <?php
+    // These are defined here so they are 'picked up' as translatable strings, because these values don't occur in the plugin itself.
+    $australia     = __( 'Australia', 'acf-city-selector' );
     $united_states = __( 'United States', 'acf-city-selector' );
