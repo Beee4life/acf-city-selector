8a9d3a582c7a39f594802cd1b6c5179dc4d73ca9
Berry Plasman <berry@totaldesign.com>
2020-05-09 22:11:20 +0200

    Change IF

--- inc/acfcs-dashboard.php
+++ inc/acfcs-dashboard.php
@@ -103,7 +103,7 @@
                         </div>
                 <?php } ?>
 
-                <?php if ( false != $show_raw_import ) { ?>
+                <?php if ( true === $show_raw_import ) { ?>
                     <?php $placeholder = "Amsterdam,NH,Noord-Holland,NL,Netherlands\nRotterdam,ZH,Zuid-Holland,NL,Netherlands"; ?>
                     <?php $submitted_raw_data = ( isset( $_POST[ 'raw_csv_import' ] ) ) ? $_POST[ 'raw_csv_import' ] : false; ?>
                     <div class="acfcs__section acfcs__section--raw-import">
