55782be78b946fc4d9b3383fc82fef5d69f741d7
Berry Plasman <berry@totaldesign.com>
2020-05-12 18:15:05 +0200

    Translate string + remove redundant values

--- assets/js/city-selector.js
+++ assets/js/city-selector.js
@@ -71,7 +71,8 @@
                             }
                             changed_state.append($stateValues);
                             // @TODO: translate string
-                            $select_city = '<option value="">Select a city</option>';
+                            var i18n_select_city = acf._e('acf_city_selector', 'i18n_select_city');
+                            $select_city = '<option value="">' + i18n_select_city + '</option>';
                             changed_city.append($select_city);
                         }
                     });
@@ -105,191 +106,6 @@
                             changed_city.append($cityValues);
                         }
                     });
-                });
-            }
-        }
-
-        /**
-         * Load select states when editing a post
-         * @TODO: remove
-         */
-        function admin_post_edit_load_states() {
-
-            if (true === Array.isArray(city_selector_vars)) {
-                // preparing the response array
-                const response_states = []
-                for (i = 0; i < city_selector_vars.length; i++) {
-                    const d = get_states(city_selector_vars[i].countryCode);
-                    response_states.push(d)
-                }
-
-                Promise.all(response_states).then(function(jsonResults) {
-                    var state_instance_count = 0;
-                    for (i = 0; i < jsonResults.length; i++) {
-                        var obj = JSON.parse(jsonResults[i]);
-                        var len = obj.length;
-                        var $stateValues = '';
-                        var select_state = $('select[name*="row-' + state_instance_count + '"][name*="stateCode"]');
-                        var stored_state = city_selector_vars[state_instance_count].stateCode;
-
-                        select_state.fadeIn();
-                        for (j = 0; j < len; j++) {
-                            $selected = '';
-                            var state = obj[j];
-                            var current_state = state.country_code + '-' + state.state_code;
-                            if (current_state === stored_state) {
-                                $selected = ' selected="selected"';
-                            }
-                            var selected = $selected;
-                            $stateValues += '<option value="' + state.country_code + '-' + state.state_code + '"' + selected + '>' + state.state_name + '</option>';
-                        }
-                        select_state.append($stateValues);
-                        state_instance_count++;
-                    }
-                });
-
-            } else {
-
-                const response_states = [];
-                if ( 'object' == typeof city_selector_vars ) {
-                    // flex block
-                    Object.size = function(obj) {
-                        var size = 0, key;
-                        for (key in obj) {
-                            if (obj.hasOwnProperty(key)) size++;
-                        }
-                        return size;
-                    };
-                    var len = Object.size(city_selector_vars);
-                    for( $i = 1; $i <= len ; $i++ ) {
-                        const d = get_states(city_selector_vars[$i].countryCode);
-                        response_states.push(d)
-                    }
-
-                } else {
-                    // single/group
-                    const d = get_states(city_selector_vars.countryCode);
-                    response_states.push(d)
-                }
-
-                Promise.all(response_states).then(function(jsonResults) {
-                    for (i = 0; i < jsonResults.length; i++) {
-                        var obj          = JSON.parse(jsonResults[i]);
-                        var len          = obj.length;
-                        var $stateValues = '';
-                        var select_state = $("select[name*='stateCode']");
-                        if ( 'object' == typeof city_selector_vars ) {
-                            $stored_state = city_selector_vars[1].stateCode;
-                        } else {
-                            $stored_state = city_selector_vars.stateCode;
-                        }
-                        var stored_state = $stored_state;
-
-                        select_state.fadeIn();
-                        for (j = 0; j < len; j++) {
-                            $selected = '';
-                            var state = obj[j];
-                            var current_state = state.country_code + '-' + state.state_code;
-                            if (current_state === stored_state) {
-                                $selected = ' selected="selected"';
-                            }
-                            var selected = $selected;
-                            $stateValues += '<option value="' + state.country_code + '-' + state.state_code + '"' + selected + '>' + state.state_name + '</option>';
-                        }
-                        select_state.append($stateValues);
-                    }
-                });
-            }
-        }
-
-        /**
-         * Load select cities when editing a post
-         * @TODO: remove
-         */
-        function admin_post_edit_load_cities() {
-            if ( true === Array.isArray(city_selector_vars) ) {
-                const response_cities = []
-                for (i = 0; i < city_selector_vars.length; i++) {
-                    const d = get_cities(city_selector_vars[i].stateCode);
-                    response_cities.push(d)
-                }
-
-                Promise.all(response_cities).then(function(jsonResults) {
-                    var city_instance_count = 0;
-                    for (i = 0; i < jsonResults.length; i++) {
-                        var obj = JSON.parse(jsonResults[i]);
-                        var len = obj.length;
-                        var $cityValues = '';
-                        var select_city = $('select[name*="row-' + city_instance_count + '"][name*="cityName"]');
-                        var stored_city = city_selector_vars[city_instance_count].cityName;
-
-                        select_city.fadeIn();
-                        for (j = 0; j < len; j++) {
-                            $selected = '';
-                            var city = obj[j];
-                            var city_name = city.city_name;
-                            if (city_name === stored_city) {
-                                $selected = ' selected="selected"';
-                            }
-                            var selected = $selected;
-                            $cityValues += '<option value="' + city_name + '"' + selected + '>' + city_name + '</option>';
-                        }
-                        select_city.append($cityValues);
-                        city_instance_count++;
-                    }
-
-                });
-
-            } else {
-
-                const response_cities = [];
-                if ( 'object' == typeof city_selector_vars ) {
-                    // flex block
-                    Object.size = function(obj) {
-                        var size = 0, key;
-                        for (key in obj) {
-                            if (obj.hasOwnProperty(key)) size++;
-                        }
-                        return size;
-                    };
-                    var len = Object.size(city_selector_vars);
-                    for( $i = 1; $i <= len ; $i++ ) {
-                        const d = get_cities(city_selector_vars[$i].stateCode);
-                        response_cities.push(d)
-                    }
-
-                } else {
-                    // single/group
-                    const d = get_cities(city_selector_vars.stateCode);
-                    response_cities.push(d)
-                }
-
-                Promise.all(response_cities).then(function(jsonResults) {
-                    for (i = 0; i < jsonResults.length; i++) {
-                        var obj = JSON.parse(jsonResults[i]);
-                        var len = obj.length;
-                        var $cityValues = '';
-                        var select_city = $('select[name*="cityName"]');
-                        if ( 'object' == typeof city_selector_vars ) {
-                            $stored_city = city_selector_vars[1].cityName;
-                        } else {
-                            $stored_city = city_selector_vars.cityName;
-                        }
-                        var stored_city = $stored_city;
-
-                        select_city.fadeIn();
-                        for (j = 0; j < len; j++) {
-                            $selected = '';
-                            var city = obj[j];
-                            var city_name = city.city_name;
-                            if (city_name === stored_city) {
-                                $selected = ' selected="selected"';
-                            }
-                            var selected = $selected;
-                            $cityValues += '<option value="' + city_name + '"' + selected + '>' + city_name + '</option>';
-                        }
-                        select_city.append($cityValues);
-                    }
                 });
             }
         }
@@ -335,11 +151,6 @@
         /**
          * Function calls
          */
-        if (typeof city_selector_vars !== "undefined") {
-            // @TODO: remove
-            // admin_post_edit_load_states();
-            // admin_post_edit_load_cities();
-        }
         change_dropdowns();
 
     });
