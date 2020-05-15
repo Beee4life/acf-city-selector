002ac4609d8c40bf940b79b25438fceb0afb48f9
Berry Plasman <berry@totaldesign.com>
2020-05-16 01:49:16 +0200

    Debug info page + function to download to json

---
+++ inc/acfcs-save-json.php
@@ -0,0 +1,12 @@
+<?php
+    /*
+     * This file handles the downloading of the JSON info
+     */
+    header('Content-disposition: attachment; filename=debug.json');
+    header('Content-type: application/json');
+
+    $json = file_get_contents( $_POST[ 'acfcs_json_file' ] );
+    echo $json;
+
+    // remove exported file
+    unlink( $_POST[ 'acfcs_json_file' ] );
