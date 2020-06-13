32a6f5893d1b9b6711abcab3ff355c5c6f9d4d83
Berry Plasman <berry@totaldesign.com>
2020-06-13 21:54:31 +0200

    Refactor

--- inc/acfcs-search.php
+++ inc/acfcs-search.php
@@ -114,7 +114,7 @@
                 " . $orderby . "
                 " . $search_limit . "
             ";
-            $cities = $wpdb->get_results( $sql );
+            $cities     = $wpdb->get_results( $sql );
             $city_array = array();
             foreach( $cities as $city_object ) {
                 $city_array[] = (array) $city_object;
