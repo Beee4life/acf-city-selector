2754d1829a83396d2fce214f4cb26ed3fcebebe4
Beee <info@berryplasman.com>
2020-09-13 02:01:39 +0200

    Rename var name

--- ACF_City_Selector.php
+++ ACF_City_Selector.php
@@ -737,8 +737,8 @@
              * @param $to_index
              */
             public static function acfcs_move_array_element( &$array, $from_index, $to_index ) {
-                $out = array_splice( $array, $from_index, 1 );
-                array_splice( $array, $to_index, 0, $out );
+                $splice = array_splice( $array, $from_index, 1 );
+                array_splice( $array, $to_index, 0, $splice );
             }
 
 
