0483926fbfddf0ebfe6497da3c76254b71fb4364
Beee <info@berryplasman.com>
2020-10-26 21:42:02 +0100

    Move textarea out of label

--- inc/acfcs-dashboard.php
+++ inc/acfcs-dashboard.php
@@ -109,9 +109,9 @@
                         </p>
                         <form method="post">
                             <input name="acfcs_import_raw_nonce" type="hidden" value="<?php echo wp_create_nonce( 'acfcs-import-raw-nonce' ); ?>" />
-                            <label>
-                                <textarea name="acfcs_raw_csv_import" id="raw-import" rows="5" placeholder="<?php echo $placeholder; ?>"><?php echo $submitted_raw_data; ?></textarea>
-                            </label>
+                            <?php // NOTE: the empty label is for HTML validity, since every input element requires a label ?>
+                            <label for="raw-import"></label>
+                            <textarea name="acfcs_raw_csv_import" id="raw-import" rows="5" placeholder="<?php echo $placeholder; ?>"><?php echo $submitted_raw_data; ?></textarea>
                             <br />
                             <input name="verify" type="submit" class="button button-primary" value="<?php esc_html_e( 'Verify CSV data', 'acf-city-selector' ); ?>" />
                             <input name="import" type="submit" class="button button-primary" value="<?php esc_html_e( 'Import CSV data', 'acf-city-selector' ); ?>" />
