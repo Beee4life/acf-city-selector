4e5b848f8f09c9e8a26dc80d93dfa93d88fac52e
Beee <info@berryplasman.com>
2020-10-27 01:49:19 +0100

    Add label

--- inc/acfcs-dashboard.php
+++ inc/acfcs-dashboard.php
@@ -108,8 +108,7 @@
                         </p>
                         <form method="post">
                             <input name="acfcs_import_raw_nonce" type="hidden" value="<?php echo wp_create_nonce( 'acfcs-import-raw-nonce' ); ?>" />
-                            <?php // NOTE: the empty label is for HTML validity, since every input element requires a label ?>
-                            <label for="raw-import"></label>
+                            <label for="raw-import"><?php esc_html_e( 'Raw CSV import', 'acf-city-selector' ); ?></label>
                             <textarea name="acfcs_raw_csv_import" id="raw-import" rows="5" placeholder="<?php echo $placeholder; ?>"><?php echo $submitted_raw_data; ?></textarea>
                             <br />
                             <input name="verify" type="submit" class="button button-primary" value="<?php esc_html_e( 'Verify CSV data', 'acf-city-selector' ); ?>" />
