f0fbb4ef3e07487f4ef12d8a3a9d0ea049120314
Beee <info@berryplasman.com>
2020-10-30 01:34:35 +0100

    Remove <p> for <ul>

--- inc/acfcs-settings.php
+++ inc/acfcs-settings.php
@@ -73,10 +73,12 @@
                     <input name="acfcs_delete_transients" value="<?php echo wp_create_nonce( 'acfcs-delete-transients-nonce' ); ?>" type="hidden" />
                     <h2><?php esc_html_e( 'Delete transients', 'acf-city-selector' ); ?></h2>
                     <p><?php esc_html_e( "If you're seeing unexpected results in your dropdowns, try clearing all transients with this option.", 'acf-city-selector' ); ?></p>
-                    <p>
-                        <label for="delete_transients" class="screen-reader-text"></label>
-                        <input type="checkbox" name="delete_transients" id="delete_transients" value="1" /> <?php esc_html_e( 'Delete all transients', 'acf-city-selector' ); ?>
-                    </p>
+                    <ul>
+                        <li>
+                            <label for="delete_transients" class="screen-reader-text"></label>
+                            <input type="checkbox" name="delete_transients" id="delete_transients" value="1" /> <?php esc_html_e( 'Delete all transients', 'acf-city-selector' ); ?>
+                        </li>
+                    </ul>
                     <input type="submit" class="button button-primary" value="<?php esc_html_e( "Delete transients", 'acf-city-selector' ); ?>" />
                 </form>
 
@@ -86,10 +88,12 @@
                     <input name="acfcs_truncate_table_nonce" value="<?php echo wp_create_nonce( 'acfcs-truncate-table-nonce' ); ?>" type="hidden" />
                     <h2><?php esc_html_e( 'Clear the database', 'acf-city-selector' ); ?></h2>
                     <p><?php esc_html_e( "By selecting this option, you will remove all cities, which are present in the database. This is useful if you don't need the preset cities or you want a fresh start.", 'acf-city-selector' ); ?></p>
-                    <p>
-                        <label for="delete_cities" class="screen-reader-text"></label>
-                        <input type="checkbox" name="delete_cities" id="delete_cities" value="1" /> <?php esc_html_e( 'Delete all cities from the database', 'acf-city-selector' ); ?>
-                    </p>
+                    <ul>
+                        <li>
+                            <label for="delete_cities" class="screen-reader-text"></label>
+                            <input type="checkbox" name="delete_cities" id="delete_cities" value="1" /> <?php esc_html_e( 'Delete all cities from the database', 'acf-city-selector' ); ?>
+                        </li>
+                    </ul>
                     <input type="submit" class="button button-primary"  onclick="return confirm( 'Are you sure you want to delete all cities ?' )" value="<?php esc_html_e( 'Delete everything', 'acf-city-selector' ); ?>" />
                 </form>
 
@@ -100,12 +104,14 @@
                     <h2><?php esc_html_e( 'Save data', 'acf-city-selector' ); ?></h2>
                     <p><?php esc_html_e( 'When the plugin is deleted, all settings and cities are deleted as well. Select this option to preserve this data upon deletion.', 'acf-city-selector' ); ?></p>
                     <?php $checked = get_option( 'acfcs_preserve_settings' ) ? ' checked="checked"' : false; ?>
-                    <p>
-                        <span class="acfcs_input">
-                            <label for="preserve_settings" class="screen-reader-text"></label>
-                            <input type="checkbox" name="preserve_settings" id="preserve_settings" value="1" <?php echo $checked; ?>/> <?php esc_html_e( 'Preserve settings on plugin deletion', 'acf-city-selector' ); ?>
-                        </span>
-                    </p>
+                    <ul>
+                        <li>
+                            <span class="acfcs_input">
+                                <label for="preserve_settings" class="screen-reader-text"></label>
+                                <input type="checkbox" name="preserve_settings" id="preserve_settings" value="1" <?php echo $checked; ?>/> <?php esc_html_e( 'Preserve settings on plugin deletion', 'acf-city-selector' ); ?>
+                            </span>
+                        </li>
+                    </ul>
                     <input type="submit" class="button button-primary" value="<?php esc_html_e( 'Save settings', 'acf-city-selector' ); ?>" />
                 </form>
             </div>
