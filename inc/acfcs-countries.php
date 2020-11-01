1a48b39cb256067f970cac46c15b7be54c51ff29
Beee <info@berryplasman.com>
2020-11-01 20:37:43 +0100

    Extend countries page with more countries/packages

--- inc/acfcs-countries.php
+++ inc/acfcs-countries.php
@@ -10,18 +10,26 @@
 
         ACF_City_Selector::acfcs_show_admin_notices();
 
-        $packages      = [];
-        $rest_packages = acfcs_get_packages();
-        if ( is_array( $rest_packages ) ) {
-            foreach( $rest_packages as $package ) {
-                $package                   = (array) $package;
-                $package[ 'country_name' ] = __( $package[ 'country_name' ], 'acf-city-selector' );
-                $packages[]                = $package;
+        $country_files = acfcs_get_packages( 'single' );
+        $country_packs = acfcs_get_packages( 'packages' );
+        $single_files  = [];
+
+        if ( is_array( $country_files ) ) {
+            foreach( $country_files as $single_file ) {
+                $single_file                   = (array) $single_file;
+                $single_file[ 'country_name' ] = __( $single_file[ 'country_name' ], 'acf-city-selector' );
+                $single_files[]                = $single_file;
             }
-            if ( ! empty( $packages ) ) {
-                usort( $packages, function( $a, $b ) {
-                    return $a[ 'country_name' ] <=> $b[ 'country_name' ];
-                } );
+            if ( ! empty( $single_files ) ) {
+                $country_name = array_column( $single_files, 'country_name' );
+                array_multisort( $country_name, SORT_ASC, $single_files );
+            }
+        }
+        if ( is_array( $country_packs ) ) {
+            foreach( $country_packs as $country_package ) {
+                $country_package                   = (array) $country_package;
+                $country_package[ 'country_name' ] = __( $country_package[ 'country_name' ], 'acf-city-selector' );
+                $country_packages[]                = $country_package;
             }
         }
         ?>
@@ -38,7 +46,7 @@
                 <div class="acfcs__section acfcs__section--gopro">
                     <h2><?php esc_html_e( 'Get countries', 'acf-city-selector' ); ?></h2>
                     <p>
-                        <?php esc_html_e( 'Default the plugin comes with 4 countries included, the Benelux (Belgium, Netherlands, Luxembourg) and Andorra but you might want to add more countries to choose from.', 'acf-city-selector' ); ?>
+                        <?php esc_html_e( 'Default the plugin comes with 2 countries included, Belgium and the Netherlands but you might want to add more countries to choose from.', 'acf-city-selector' ); ?>
                     </p>
                     <p>
                         <?php esc_html_e( 'And now you can !! We have created several \'country packages\' for you to import as is.', 'acf-city-selector' ); ?>
@@ -46,22 +54,27 @@
                 </div>
 
                 <div class="acfcs__section acfcs__section--packages">
-                    <h2><?php esc_html_e( 'Country packages', 'acf-city-selector' ); ?></h2>
+                    <?php if ( is_array( $single_files ) && ! empty( $single_files ) ) { ?>
+                        <h2>
+                            <?php esc_html_e( 'Country files', 'acf-city-selector' ); ?>
+                        </h2>
 
-                    <table class="acfcs__table acfcs__table--packages">
-                        <thead>
-                        <tr>
-                            <th>&nbsp;</th>
-                            <th><?php esc_html_e( 'Country', 'acf-city-selector' ); ?></th>
-                            <th># <?php esc_html_e( 'States/Provinces', 'acf-city-selector' ); ?></th>
-                            <th># <?php esc_html_e( 'Cities', 'acf-city-selector' ); ?></th>
-                            <th><?php esc_html_e( 'Price', 'acf-city-selector' ); ?></th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php if ( is_array( $packages ) ) { ?>
+                        <div class="hide400">
+                            <?php esc_html_e( 'Rotate your phone for a better view or scroll the list horizontally.', 'acf-city-selector' ); ?>
+                        </div>
+
+                        <table class="acfcs__table acfcs__table--packages scrollable">
+                            <thead>
+                            <tr>
+                                <th colspan="2"><?php esc_html_e( 'Country', 'acf-city-selector' ); ?></th>
+                                <th># <?php esc_html_e( 'States/Provinces', 'acf-city-selector' ); ?></th>
+                                <th># <?php esc_html_e( 'Cities', 'acf-city-selector' ); ?></th>
+                                <th><?php esc_html_e( 'Price', 'acf-city-selector' ); ?></th>
+                            </tr>
+                            </thead>
+                            <tbody>
                             <?php $total_price = 0; ?>
-                            <?php foreach( $packages as $package ) { ?>
+                            <?php foreach( $single_files as $package ) { ?>
                                 <?php $total_price = ( ! empty( $package[ 'price' ] ) ) ? $total_price + $package[ 'price' ] : $total_price; ?>
                                 <tr>
                                     <td><img src="<?php echo ACFCS_PLUGIN_URL . 'assets/img/flags/' . $package[ 'country_code' ] . '.png'; ?>" alt="" /></td>
@@ -87,9 +100,55 @@
                                     </td>
                                 </tr>
                             <?php } ?>
-                        <?php } ?>
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    <?php } ?>
+
+                    <?php if ( is_array( $country_packages ) && ! empty( $country_packages ) ) { ?>
+
+                        <h2><?php esc_html_e( 'Combined country packages', 'acf-city-selector' ); ?></h2>
+
+                        <table class="acfcs__table acfcs__table--packages">
+                            <thead>
+                            <tr>
+                                <th><?php esc_html_e( 'Package', 'acf-city-selector' ); ?></th>
+                                <th><?php esc_html_e( 'Included countries', 'acf-city-selector' ); ?></th>
+                                <th><?php esc_html_e( 'Price', 'acf-city-selector' ); ?></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php $total_price = 0; ?>
+                            <?php foreach( $country_packages as $package ) { ?>
+                                <?php $total_price = ( ! empty( $package[ 'price' ] ) ) ? $total_price + $package[ 'price' ] : $total_price; ?>
+                                <tr>
+                                    <td>
+                                        <?php echo __( $package[ 'country_name' ], 'acf-city-selector' ); ?>
+                                    </td>
+
+                                    <td>
+                                        <?php
+                                            if ( isset( $package[ 'included_countries' ] ) && is_array( $package[ 'included_countries' ] ) && ! empty( $package[ 'included_countries' ] ) ) {
+                                                foreach( $package[ 'included_countries' ] as $country_code ) {
+                                                    echo '<img src="' . ACFCS_PLUGIN_URL . 'assets/img/flags/' . $country_code . '.png" alt="' . $country_code . '" class="flag" />';
+                                                }
+                                            }
+                                        ?>
+                                    </td>
+
+                                    <td>
+                                        <?php
+                                            if ( ! empty( $package[ 'price' ] ) ) {
+                                                echo '&euro; ' . $package[ 'price' ] . ',00';
+                                            } else {
+                                                echo sprintf( '<a href="%s" target="_blank" rel="noopener">%s</a>', ACFCS_WEBSITE_URL . '/get-countries/?utm_source=wpadmin&utm_medium=free_download&utm_campaign=acf-plugin', __( 'Free', 'acf-city-selector' ) );
+                                            }
+                                        ?>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                            </tbody>
+                        </table>
+                    <?php } ?>
 
                     <p>
                         <?php echo sprintf( __( 'More countries will be added soon. Feel free to <a href="%s" target="_blank" rel="noopener">request</a> a country, if it\'s not available (yet).', 'acf-city-selector' ), esc_url( 'https://github.com/Beee4life/acf-city-selector/issues' ) ); ?>
