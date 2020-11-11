38928f1f4a5231dbc74bd073801d7a17a65ea4ca
Beee <info@berryplasman.com>
2020-11-11 04:55:26 +0100

    New i18n strings

--- inc/acfcs-i18n.php
+++ inc/acfcs-i18n.php
@@ -1,11 +1,16 @@
 <?php
     // These are defined here so they are 'picked up' as translatable strings, because these values don't occur in the plugin itself.
+    $andorra       = __( 'Andorra', 'acf-city-selector' );
+    $austria       = __( 'Austria', 'acf-city-selector' );
     $australia     = __( 'Australia', 'acf-city-selector' );
     $brazil        = __( 'Brazil', 'acf-city-selector' );
     $canada        = __( 'Canada', 'acf-city-selector' );
+    $europe        = __( 'Europe', 'acf-city-selector' );
     $france        = __( 'France', 'acf-city-selector' );
     $germany       = __( 'Germany', 'acf-city-selector' );
+    $greatbritain  = __( 'Great Britain', 'acf-city-selector' );
     $luxembourg    = __( 'Luxembourg', 'acf-city-selector' );
+    $mexico        = __( 'Mexico', 'acf-city-selector' );
     $spain         = __( 'Spain', 'acf-city-selector' );
     $switzerland   = __( 'Switzerland', 'acf-city-selector' );
     $united_states = __( 'United States', 'acf-city-selector' );
