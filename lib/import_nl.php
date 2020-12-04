dbe4ae2b0461aa04dbc1235055d360ab6f40ba5f
Beee <info@berryplasman.com>
2020-12-04 16:17:32 +0100

    Remove redundant last line

--- lib/import_nl.php
+++ lib/import_nl.php
@@ -2,4 +2,3 @@
     include 'cities_nl.php';
 
     $this->acfcs_errors()->add( 'success_cities_imported_netherlands', esc_html__( 'Successfully imported 2449 cities for the Netherlands.', 'acf-city-selector' ) );
-
