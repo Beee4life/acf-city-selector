8d620e90c9f78ebc2fcdb64e0f91b0c64925b65f
Beee <info@berryplasman.com>
2020-12-11 03:40:48 +0100

    Remove lib files

--- lib/import_be.php
+++
@@ -1,4 +0,0 @@
-<?php
-    include 'cities_be.php';
-
-    $this->acfcs_errors()->add( 'success_cities_imported_belgium', esc_html__( 'Successfully imported 1166 cities for Belgium.', 'acf-city-selector' ) );
--- lib/import_nl.php
+++
@@ -1,4 +0,0 @@
-<?php
-    include 'cities_nl.php';
-
-    $this->acfcs_errors()->add( 'success_cities_imported_netherlands', esc_html__( 'Successfully imported 2449 cities for the Netherlands.', 'acf-city-selector' ) );
