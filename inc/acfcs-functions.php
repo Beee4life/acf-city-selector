6cbdc75527e1522a4bda6e57793b64578d719ac4
Beee <info@berryplasman.com>
2020-12-16 18:06:42 +0100

    Add backticks

--- inc/acfcs-functions.php
+++ inc/acfcs-functions.php
@@ -726,7 +726,7 @@
         if ( ! empty( $sanitized_country_codes ) ) {
             global $wpdb;
             $country_string = strtoupper( "'" . implode( "', '", $sanitized_country_codes ) . "'" );
-            $query          = "DELETE FROM {$wpdb->prefix}cities WHERE country_code IN ({$country_string})";
+            $query          = "DELETE FROM {$wpdb->prefix}cities WHERE `country_code` IN ({$country_string})";
             $result         = $wpdb->query( $query );
             if ( $result > 0 ) {
                 ACF_City_Selector::acfcs_errors()->add( 'success_country_remove', sprintf( esc_html__( 'You have successfully removed all entries for %s.', 'acf-city-selector' ), $country_names_and ) );
