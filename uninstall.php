700e7168f562cb1822c3a7893014bbbdc7d60a33
Beee <info@berryplasman.com>
2021-04-23 23:02:27 +0200

    Change option/setting

--- uninstall.php
+++ uninstall.php
@@ -4,8 +4,7 @@
         die();
     }
 
-    if ( false == get_option( 'acfcs_preserve_settings' ) ) {
-
+    if ( false != get_option( 'acfcs_delete_cities_table' ) ) {
         // drop table
         global $wpdb;
         $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}cities");
