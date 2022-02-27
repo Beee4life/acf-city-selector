82c351d5666b269e9f73e4a2a12fee29d0ed3a4d
Beee <info@berryplasman.com>
2022-02-27 14:57:32 +0100

    Add custom message to validation

--- assets/js/field-validation.js
+++ assets/js/field-validation.js
@@ -1,21 +1,29 @@
 // for v4
-(function($) {
-    $(document).on('acf/validate_field', function (e, field) {
+(function ($) {
+  $(document).on('acf/validate_field', function (e, field) {
 
-        $field = $(field);
-        $country_value = $field.find('select#acf-field-city_selectorcountryCode').val();
-        $state_value = $field.find('select#acf-field-city_selectorstateCode').val();
-        $city_value = $field.find('select#acf-field-city_selectorcityName').val();
+    $field = $(field);
+    $country_field = $field.find('select#acf-field-city_selectorcountryCode');
+    $country_value = $country_field.val();
 
-        if ($country_value === '' || typeof($country_value) === "undefined" ) {
-            $field.data('validation', false);
-        }
-        if ($state_value === '' || typeof($state_value) === "undefined" ) {
-            $field.data('validation', false);
-        }
-        if ($city_value === '' || typeof($city_value) === "undefined" ) {
-            $field.data('validation', false);
-        }
+    $state_field = $field.find('select#acf-field-city_selectorstateCode');
+    $state_value = $state_field.val();
 
-    });
+    $city_field = $field.find('select#acf-field-city_selectorcityName');
+    $city_value = $city_field.val();
+
+    if ($country_value === '' || typeof ($country_value) === "undefined") {
+      $field.data('validation', false);
+      $field.data('validation_message', acf.l10n.validation.select_country);
+    }
+    if ($state_value === '' || typeof ($state_value) === "undefined") {
+      $field.data('validation', false);
+      $field.data('validation_message', acf.l10n.validation.select_state);
+    }
+    if ($city_value === '' || typeof ($city_value) === "undefined") {
+      $field.data('validation', false);
+      $field.data('validation_message', acf.l10n.validation.select_city);
+    }
+
+  });
 })(jQuery);
