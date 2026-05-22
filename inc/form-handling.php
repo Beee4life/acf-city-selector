99b38dbcfac5c57730b82af8805284a74e66ab49
Beee <info@berryplasman.com>
2026-05-22 23:20:53 +0200

    Improve delete country

--- inc/form-handling.php
+++ inc/form-handling.php
@@ -99,12 +99,17 @@
                 if ( empty( $_POST[ 'acfcs_delete_country' ] ) ) {
                     ACF_City_Selector::acfcs_errors()->add( 'error_no_country_selected', esc_html__( "You didn't select any countries, please try again.", 'acf-city-selector' ) );
                 } else {
-                    if ( is_array( $_POST[ 'acfcs_delete_country' ] ) ) {
-                        foreach( wp_unslash($_POST[ 'acfcs_delete_country' ] ) as $country_code ) {
-                            $country_codes[] = sanitize_text_field( $country_code );
-                        }
-                        if ( ! empty( $country_codes ) ) {
-                            acfcs_delete_country( $country_codes );
+
+                    if ( isset( $_POST[ 'acfcs_delete_country' ] ) ) {
+                        // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+                        $raw_country_input = wp_unslash( $_POST[ 'acfcs_delete_country' ] );
+                        if ( is_array( $raw_country_input ) ) {
+                            $country_codes = array_map( 'sanitize_text_field', $raw_country_input );
+                            $country_codes = array_filter( $country_codes );
+
+                            if ( ! empty( $country_codes ) ) {
+                                acfcs_delete_country( $country_codes );
+                            }
                         }
                     }
                 }
